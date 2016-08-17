558718f6c9ee35205bc3456f9b4f92454f6d45c4
trunks001 <gfzondi@gmail.com>
2016-08-17 07:47:04 +0200

    Bataung Events Form for BMW Event

---
+++ Spon_Dev/Bataung_Events_Form/data.php
@@ -0,0 +1,28 @@
+ <?php
+$servername = "localhost";
+$username = "checkers";
+$password = "checkers123#";
+$dbname = "bataungbmw";
+$title1 = $_POST['title'];
+$fname1 = $_POST['fname'];
+$lname = $_POST['surname'];
+$contact2 = $_POST['contact1'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
