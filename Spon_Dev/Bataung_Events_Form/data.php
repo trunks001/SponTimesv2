140a568f99f5d5797de3a0f40b37543374db6ae7
trunks001 <gfzondi@gmail.com>
2016-08-22 17:24:40 +0200

    Latest version of the Bataung Events Form

--- Spon_Dev/Bataung_Events_Form/data.php
+++ Spon_Dev/Bataung_Events_Form/data.php
@@ -1,28 +1,89 @@
- <?php
-$servername = "localhost";
-$username = "checkers";
-$password = "checkers123#";
-$dbname = "bataungbmw";
-$title1 = $_POST['title'];
-$fname1 = $_POST['fname'];
-$lname = $_POST['surname'];
-$contact2 = $_POST['contact1'];
+<?php
+ /* Attempt MySQL server connection. Assuming you are running MySQL
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    server with default setting (user 'root' with no password) */
+
+    $link = mysqli_connect("localhost", "bataungb_gcina", "Trunkswilltry001!", "bataungb_attendees");
+
+     
+
+    // Check connection
+
+    if($link === false){
+
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+
+    }
+
+     
+
+    // Escape user inputs for security
+
+    $title = mysqli_real_escape_string($link, $_POST['title']);
+    
+    $first_name = mysqli_real_escape_string($link, $_POST['firstname']);
+
+    $last_name = mysqli_real_escape_string($link, $_POST['lastname']);
+
+    $email_address = mysqli_real_escape_string($link, $_POST['email']);
+    
+    $telephone = mysqli_real_escape_string($link, $_POST['telephone']);
+    
+    $cellphone = mysqli_real_escape_string($link, $_POST['cellphone']);
+    
+    $fax = mysqli_real_escape_string($link, $_POST['fax']);
+    
+    $company_name = mysqli_real_escape_string($link, $_POST['companyname']);
+    
+    $department = mysqli_real_escape_string($link, $_POST['department']);
+    
+    $designation = mysqli_real_escape_string($link, $_POST['designation']);
+    
+    $event_date = mysqli_real_escape_string($link, $_POST['eventdate']);
+    
+    $diet = mysqli_real_escape_string($link, $_POST['diet']);
+    
+    $allergies = mysqli_real_escape_string($link, $_POST['allergies']);
+    
+    $response = mysqli_real_escape_string($link, $_POST['submit']);    
+     
+
+    // attempt insert query execution
+
+    $sql = "INSERT INTO attendees (title, firstName, lastName, email, telephone, cellphone, fax, companyName, department, designation, eventDate, diet, allergies, response) "
+            . "VALUES ('$title', '$first_name', '$last_name', '$email_address', '$telephone', '$cellphone', '$fax', '$company_name', '$department', '$designation', '$event_date', '$diet', '$allergies', '$response')";
+
+    
+   
+if(mysqli_query($link, $sql)){
+
+       echo "Records added successfully.";
+       if ($_POST['submit'] == 'Accept') {
+        //action for Accept here
+           header("Location: ThankYou.html");
+            die();           
+        } else if ($_POST['submit'] == 'Decline') {
+        //action for Decline
+           header("Location: Decline.html");
+            die();
+        } else {
+        //action for Tentative
+           header("Location: Tentative.html");
+            die();
 }
 
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
+    } else{
 
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+
 }
 
-$conn->close();
-?> 
+     
+
+    // close connection
+
+    mysqli_close($link);
+
+    
+    ?>
+
